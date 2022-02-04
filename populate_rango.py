45887ac697857aebcbcb80b4894afe09d4af5e94
Niamh Boyle <2566489b@student.gla.ac.uk>
2022-02-04 10:52:48 +0000

    finished chap 5.8

--- populate_rango.py
+++ populate_rango.py
@@ -46,7 +46,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat)
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['category'], p['url'])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
@@ -61,8 +61,8 @@ def add_page(cat, title, url, views=0):
     return p
 
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_cat(name, views=0, likes=0):
+    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
     c.save()
     return c
 
